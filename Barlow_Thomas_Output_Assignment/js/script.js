7bde99891005cbeb6ebd409e2477f360b4159164
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-04 07:19:10 -0500

    Added comment with instructions for the assignment

--- Barlow_Thomas_Output_Assignment/js/script.js
+++ Barlow_Thomas_Output_Assignment/js/script.js
@@ -2,3 +2,13 @@
 // SDI - Section 2
 // March 4, 2015
 // Output Assignment
+
+/* Brainstorm an original idea for some variables you can create and values you can
+   assign that tell me something about you.  Your code should include the following:
+   Comments
+   An example of a string variable
+   An example of a number variable
+   An example of a Boolean variable
+   Output for each variable
+*/
+
