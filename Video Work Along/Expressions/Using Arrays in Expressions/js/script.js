f56b3570ee0ce4f8964d34091b1862fd8a9be114
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 08:58:25 -0500

    finished "Arrays in Expressions" screencast

--- Video Work Along/Expressions/Using Arrays in Expressions/js/script.js
+++ Video Work Along/Expressions/Using Arrays in Expressions/js/script.js
@@ -1,1 +1,6 @@
-alert("This is in the js file");
+//Expressions - Using arrays in Expressions
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
