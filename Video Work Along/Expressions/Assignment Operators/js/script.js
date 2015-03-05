de003339ad3b26e9d41f739def8c7dd545d23312
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 11:21:28 -0500

    finished "Assignment Operators" screencast

--- Video Work Along/Expressions/Assignment Operators/js/script.js
+++ Video Work Along/Expressions/Assignment Operators/js/script.js
@@ -1,1 +1,11 @@
-alert("This is in the js file");
+// Expressions - Assignment Operators
+
+var a = 3;
+
+a -= 4; // a = a / 4
+a += 4; // a = a + 4
+a++; // a = a + 1
+a--; // a = a - 1
+
+
+console.log(a)
