a7fd99da7ccaff37fc0b3bf6a5c0198c9095467d
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 08:53:37 -0500

    finished "Order of Operations" Video

--- Video Work Along/Expressions/Order of Operations/js/script.js
+++ Video Work Along/Expressions/Order of Operations/js/script.js
@@ -1,1 +1,10 @@
-alert("This is in the js file");
+// Expressions - PEMDAS - The Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+// The average is the sum of the items ivied by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
+console.log(average);
