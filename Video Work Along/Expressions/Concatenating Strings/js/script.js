d4b69d5f748c57b9d68649901314c8f7161e0c34
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 09:04:24 -0500

    finished "Concatenating Strings" screencast

--- Video Work Along/Expressions/Concatenating Strings/js/script.js
+++ Video Work Along/Expressions/Concatenating Strings/js/script.js
@@ -1,1 +1,13 @@
-alert("This is in the js file");
+//Expressions - Concatenating Strings
+
+var firstName = "Thomas";
+var lastName = "Barlow";
+
+var fullName;
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
