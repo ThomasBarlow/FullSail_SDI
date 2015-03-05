5a5ad94d3b587f0f36400af06cc86c41ac6cd2dc
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 08:44:26 -0500

    finished "Example Expressions" screencast

--- Video Work Along/Expressions/Arithmetic Operators/js/script.js
+++ Video Work Along/Expressions/Arithmetic Operators/js/script.js
@@ -1,1 +1,10 @@
-alert("This is in the js file");
+// Expression - Arithmetic Operators
+
+// area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area;
+
+area = width * height / 2;
+
+console.log(area);
