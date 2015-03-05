9eac914e0c7936db54cb2e218266227685674b9c
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-05 08:35:46 -0500

    finished first video "Beginning Expressions"

--- Video Work Along/Expressions/Expressions/js/script.js
+++ Video Work Along/Expressions/Expressions/js/script.js
@@ -1,1 +1,6 @@
-alert("This is in the js file");
+var a = 2; // Sets up our variable and defines it with 2
+var b;
+b = a + 3 // adding 3 to a and storing it in b
+a = a + 3 // adding 3 to a
+console.log(a);
+
