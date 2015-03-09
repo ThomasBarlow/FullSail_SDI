ad85f05289a612edde2b9554f9e74d75ad1d26e5
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-09 13:30:59 -0400

    Finished the "Pizza per Person" and "Pizza for Sparky" problems

--- Expression_Worksheet/js/script.js
+++ Expression_Worksheet/js/script.js
@@ -30,3 +30,33 @@ function dogYears(dogAge) {
 
 // Call the dogYears function
 dogYears(4);
+
+/* Calculate amount of pizza slices everyone ate at a party */
+
+// Givens are Number of slices per pizza, number of people at the party, and the number of pizzas ordered
+// Create a function for the pizza math
+function slicesPerPerson(pies, slicesPerPie, numberOfPeople) {
+	// The math is number of pies x number of slices divided by number of people
+	// Note that the parenthesis are not needed, they are only there to seperate the information
+	var amount = (pies * slicesPerPie) / numberOfPeople;
+	// log the message to the console
+	console.log("Each person ate " + amount + " slices of pizza at the party");
+};
+
+// Call the slicesPerPerson function
+slicesPerPerson(10, 8, 20);
+
+/* Calculate amount of pizza slices Sparky gets to eat at the party */
+
+// Givens are Number of slices per pizza, number of people at the party, and the number of pizzas ordered
+// Create a function for the pizza math
+function slicesForSparky(pies, slicesPerPie, numberOfPeople) {
+	// The math is number of pies x number of slices then use the remainder of operator for Sparky's amount
+	// Note that the parenthesis are not needed, they are only there to seperate the information
+	var amount = (pies * slicesPerPie) % numberOfPeople;
+	// log the message to the console
+	console.log("Sparky got " + amount + " slices of pizza.");
+};
+
+// Call the slicesForSparky function
+slicesForSparky(4, 8, 10);
