fdf6e492f9eafe578ccae6301bee441930ca0e26
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-09 13:15:35 -0400

    Finished first example "Area of rectangle"

--- Expression_Worksheet/js/script.js
+++ Expression_Worksheet/js/script.js
@@ -1,3 +1,18 @@
-// Thomas Barlow
-// March 9, 2015
-// Expression Worksheet
+/* 
+Thomas Barlow
+March 9, 2015
+Expression Worksheet
+*/
+
+// Calculate the area of a rectangle given the width and height
+
+// Create a function for area of rectangle with width and height as parameters
+function rectangleArea(width, height) {
+	// calculate the width x height and place in area var
+	var area = width * height;
+	// log the results to the console
+	console.log("The area of the rectangle is " + area + ".");
+}; 
+
+// call the function with two parameters
+rectangleArea(10, 10);
