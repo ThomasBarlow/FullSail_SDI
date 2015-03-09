b2c35ec73e95cd56afdd3232f670342680b220d4
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-09 13:19:56 -0400

    Finished the "Dog Years" problem

--- Expression_Worksheet/js/script.js
+++ Expression_Worksheet/js/script.js
@@ -4,7 +4,7 @@ March 9, 2015
 Expression Worksheet
 */
 
-// Calculate the area of a rectangle given the width and height
+/* Calculate the area of a rectangle given the width and height */
 
 // Create a function for area of rectangle with width and height as parameters
 function rectangleArea(width, height) {
@@ -16,3 +16,17 @@ function rectangleArea(width, height) {
 
 // call the function with two parameters
 rectangleArea(10, 10);
+
+/* Calculate how old Sparky the pit bull is in dog years */
+
+// Givens are Sparky's age and the results should be Sparky's age in dog years
+// Create a function for the Sparky math
+function dogYears(dogAge) {
+	//do the math and store in age var
+	var age = dogAge * 7;
+	// log the results to the console with original and new values
+	console.log("Sparky is " + dogAge + " human years old which is " + age + " in dog years");
+};
+
+// Call the dogYears function
+dogYears(4);
