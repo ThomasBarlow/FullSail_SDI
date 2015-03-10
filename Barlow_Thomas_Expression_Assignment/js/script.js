b4c852d1d825d8040e6d643e17f6b94d1fbd8516
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-10 11:24:53 -0400

    Added variables with comments for each

--- Barlow_Thomas_Expression_Assignment/js/script.js
+++ Barlow_Thomas_Expression_Assignment/js/script.js
@@ -7,5 +7,14 @@ Expression Assignment
 // This code will take information from a user about 4 grades they have and will compute the average of
 // the grade total based on the 4 grades entered.
 
+/* Variables */
 
+// This is the gradeArray that will hold all of the values for the grades 
+var gradesArray = [];
+// numberOfGrades will hold the total amount of individual grades the user entered
+var numberOfGrades = 4;
+// gradesTotal will hold the added total of all the grades entered
+var gradesTotal = 0;
+// averageOfGrades will hold the mathmatical output of gradesTotal divided by numberOfGrades
+var averageOfGrades;
 
