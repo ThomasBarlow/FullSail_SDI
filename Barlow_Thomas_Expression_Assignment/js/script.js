58b17ab26bfeaf705367568812e6099818ba89ba
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-10 11:21:44 -0400

    Created files for "Expression Assignment"

---
+++ Barlow_Thomas_Expression_Assignment/js/script.js
@@ -0,0 +1,11 @@
+/*
+Thomas Barlow
+March 10, 2015
+Expression Assignment
+*/
+
+// This code will take information from a user about 4 grades they have and will compute the average of
+// the grade total based on the 4 grades entered.
+
+
+
