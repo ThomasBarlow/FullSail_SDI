15c3e93ba5c3411603ab7f05f1c670e3d6311146
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-12 16:50:47 -0400

    Finished Conditionals with expressions screencast

--- Video Work Along/Week 3/Condition with Expression/js/script.js
+++ Video Work Along/Week 3/Condition with Expression/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+// conditionals with expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+var sneakerLift = 2;
+
+if (kidHeight + sneakerLift > minHeight) {
+	console.log("You can ride the coaster");
+}
