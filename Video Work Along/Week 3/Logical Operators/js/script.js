6fd09791351ade7273c5f6aaebc1b8dc1a0446b1
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-12 17:54:44 -0400

    Finished Logical Operators screencast

--- Video Work Along/Week 3/Logical Operators/js/script.js
+++ Video Work Along/Week 3/Logical Operators/js/script.js
@@ -1,1 +1,13 @@
-alert("This is in the js file");
+// Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+
+
+if(iPhonePrice < budget && paycheck > 300) {
+	console.log("We can buy the phone!");
+} else {
+	console.log("No phone for you!");
+}
+
