0e05398614df8d49eadec22e24e4c571a77abe06
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-12 16:59:09 -0400

    Finished "Else If" screencast

--- Video Work Along/Week 3/else if/js/script.js
+++ Video Work Along/Week 3/else if/js/script.js
@@ -1,1 +1,15 @@
-alert("This is in the js file");
+// Else If
+
+var kidHeight = 52;
+var minHeight = 48;
+var parentHeight = 45;
+
+
+if (kidHeight > minHeight) {
+	console.log("You can ride");
+} else if (kidHeight >= parentHeight) {
+	// You can ride with a parent
+	console.log("You can ride with a parent");
+} else {
+	console.log("Gotta grow buddy!");
+}
