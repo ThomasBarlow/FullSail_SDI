eb6fbac3aa890d94383ebaacc42d88eadb9a5233
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-18 13:13:56 -0400

    Continue working on user input, while validating the input.  Checked code with console.log just to make sure everything is coming out

--- Barlow_Thomas_Conditionals_Assignment/js/script.js
+++ Barlow_Thomas_Conditionals_Assignment/js/script.js
@@ -10,11 +10,11 @@
 
 // Create variables to hold the three values of the three angles
 // Angle A
-var angleA = 0;
+var angleA = "";
 // Angle B
-var angleB = 0;
+var angleB = "";
 // Angle C
-var angleC = 0;
+var angleC = "";
 
 // Since we need to use the ternary conditional with strings I will ask the user for some 
 // addiotional input
@@ -39,6 +39,28 @@ timeOfDay = Number(prompt("What is the current hour (in 24h time)"));
 // If it is less then or equal to 12 (meaning 1am to 12pm) it will say goodmoring
 // If it is anything else if will say goodevening
 timeOfDayMessage = (timeOfDay < 12) ? "Goodmorning " + name + "," : "Goodevening " + name + ",";
+
+// Prompt for angleA while converting it to a number with a while statement to validate it
+while (angleA === "" || angleA === 0) {
+	angleA = Number(prompt("What is the first angle:"));
+}
+// Prompt for angleB while converting it to a number with a while statement to validate it
+while (angleB === "" || angleB === 0){
+	angleB = Number(prompt("What is the second angle:"));
+}
+// Prompt for angleC while converting it to a number with a while statement to validate it
+while (angleC === "" || angleC === 0) {
+	angleC = Number(prompt("What is the third angle:"));
+}
+
+console.log(angleA + " " + angleB + " " + angleC);
+
+
+
+
+
+
+
 
 
 
