c0858e85a05a299ba90bc772da2d534fa20399e4
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-23 08:03:41 -0400

    Finished writing the functions for the lottery program

--- Barlow_Thomas_Functions_Assignment/js/script.js
+++ Barlow_Thomas_Functions_Assignment/js/script.js
@@ -10,8 +10,13 @@ Function Assignment
 var userFloridaNumbers = new Array(6);
 // Create an array to hold the user entered PowerBall numbers
 var userPowerBallNumbers = new Array(6);
+// Create array to hold random Florida numbers
+var randomFloridaLottery = new Array(6);
+// create array to hold random PowerBall numbers
+var randomPowerBallLottery = new Array(6);
 
-if (confirm("Do you want to play the Florida Lottory?")) {
+// Use an if statement to see if the user wants to play the Florida Lottery
+if (confirm("Do you want to play the Florida Lottery?")) {
 
 	/* Floria User Input */
 	// Create a for loop to enter values in the userFloridaNumbers array
@@ -24,6 +29,8 @@ if (confirm("Do you want to play the Florida Lottory?")) {
 			userFloridaNumbers[i] = Number(prompt("Please enter a VALID number from 1 - 53: "));
 		}
 	}
+	playFloridaLottery(userFloridaNumbers);
+  // And an else if if they want to play the PowerBall Lottery
 } else if (confirm("Do you want to play the PowerBall Lotery?")) {
 	/* PowerBall User Input */
 	// Create a for loop to enter values in the userPowerBallNumbers array
@@ -43,16 +50,49 @@ if (confirm("Do you want to play the Florida Lottory?")) {
 		// If the user did not enter valid input, ask again with a different statement
 		userPowerBallNumbers[5] = Number(prompt("Please enter a VALID PowerBall FINAL number: Please enter a number between 1 - 35:"));
 	}
+	playPowerBallLottery(randomPowerBallLottery);
 } 
 /********* End User Input Section *********/
 
+/********* Begin Random Lottery Functions *********/
+
+
+
+function playFloridaLottery(array) {
+	for (var i = 0; i < array.length; i++) {
+		array[i] = Math.ceil(Math.random() * 53);
+	}
+	console.log("The Florida Lottery Numbers are: " + array[0] + " " + array[1] + " " + array[2] + " " + array[3] + " " +
+	        										  array[4] + " " + array[5] + "\n" +
+	       	    "Your Florida Lottery Numbers are: " + userFloridaNumbers[0] + " " + userFloridaNumbers[1] + " " + userFloridaNumbers[3] + " " + 
+	        									   userFloridaNumbers[4] + " " + userFloridaNumbers[5]);
+	return array;
+}
+
+
+
+
+function playPowerBallLottery(array) {
+	for (var i = 0; i < array.length - 1; i++) {
+		array[i] = Math.ceil(Math.random() * 59);
+	}
+	array[5] = Math.ceil(Math.random() * 35);
+	console.log("The PowerBall Numbers are: " + array[0] + " " + array[1] + " " + array[2] + " " + array[3] + " " + 
+												array[4] + " " + "And the final PowerBall Number is: " + array[5] + "\n" +
+			    "Your PowerBall Numbers are " + userPowerBallNumbers[0] + " " + userPowerBallNumbers[1] + " " + userPowerBallNumbers[2] + " " + 
+											    userPowerBallNumbers[3] + " " + userPowerBallNumbers[4] + " " + 
+				"And your final PowerBall Number is: " + userPowerBallNumbers[5]);
+	return array;
+}
+
+
+
+/********* End Random Lottery Functions *********/
 
 
 
 
 
 
-//for (var i = 0; i < array.length; i++) {
-//	array[i] = Math.ceil(Math.random() * 59));
-//	
-//}
+
+
