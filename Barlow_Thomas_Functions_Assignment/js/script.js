6ccdbb1644f87b6759a7bb662db2f7a05d9d7671
Thomas Barlow <tbarlow@fullsail.edu>
2015-03-24 13:01:18 -0400

    Few last minute changes

--- Barlow_Thomas_Functions_Assignment/js/script.js
+++ Barlow_Thomas_Functions_Assignment/js/script.js
@@ -4,12 +4,46 @@ March 22, 2015
 Function Assignment
 */
 
+/******** Variables ********/
+
 // Variable to hold the floridaLottery numbers
-var floridaLottery = [];
+var newFloridaLottery = [];
 // Variable to hold the powerBallLottery numbers
-var powerBallLottery = [];
+var newPowerBallLottery = [];
 // Create a variable for the user prompt
 var userPrompt;
+// variable to hold the result of the function
+var floridaLottery ;
+// variable to hold the result of the function
+var powerBallLottery;
+
+/******** User Input If and While statements ********/
+
+// Prompt the user to enter a value for the userPrompt variable converting the string to lowercase
+userPrompt = prompt("Please enter either 'FL' for the Florida Lottery or 'PB' for the PowerBall").toLowerCase();
+// While loop to check if the user entered an empty string
+while (userPrompt == "") {
+	// if an empty string is entered loop for a new response converting the string to lowercase
+	userPrompt = prompt("Please enter a valid response 'FL' or 'PB'").toLowerCase();
+}
+// If loop to run the function if the user wants the Florida Lottery
+if (userPrompt == 'fl') {
+	// Call the lottery function passing the floridaLottery array
+	floridaLottery = playFloridaLottery(newFloridaLottery);
+	// Output the results to the console using the updated floridaLottery array
+	console.log("The Florida Lottery Numbers are: " + floridaLottery[0] + " " + floridaLottery[1] + " " + floridaLottery[2] + 
+												" " + floridaLottery[3] + " " + floridaLottery[4] + " " + floridaLottery[5]);
+  // else if they want the PowerBall Lottery		
+} else if (userPrompt == 'pb') {
+	// call the powerBallLottery function passing the powerBallLottery array
+	powerBallLottery = playPowerBallLottery(newPowerBallLottery);
+	// Log to the console the results of the array
+	console.log("The PowerBall Numbers are: " + powerBallLottery[0] + " " + powerBallLottery[1] + " " + powerBallLottery[2] + 
+										  " " + powerBallLottery[3] + " " + powerBallLottery[4] + "\n" 
+										      + "And the final PowerBall Number is: " + powerBallLottery[5]);
+}
+
+/******** Functions ********/
 
 // Create a function for the Florida Lottery
 function playFloridaLottery(array) {
@@ -68,39 +102,6 @@ function playPowerBallLottery(array) {
 	// return the array so that it may be assigned into a variable		    
 	return array;
 }
-
-// Prompt the user to enter a value for the userPrompt variable converting the string to lowercase
-userPrompt = prompt("Please enter either 'FL' for the Florida Lottery or 'PB' for the PowerBall").toLowerCase();
-// While loop to check if the user entered an empty string
-while (userPrompt == "") {
-	// if an empty string is entered loop for a new response converting the string to lowercase
-	userPrompt = prompt("Please enter a valid response 'FL' or 'PB'").toLowerCase();
-}
-
-/*
-while (userPrompt != 'fl' || userPrompt != "pb") {
-	userPrompt = prompt("Please enter only 'FL' or 'PB'");
-}
-*/
-
-// If loop to run the function if the user wants the Florida Lottery
-if (userPrompt == 'fl') {
-	// Call the lottery function passing the floridaLottery array
-	playFloridaLottery(floridaLottery);
-	// Output the results to the console using the updated floridaLottery array
-	console.log("The Florida Lottery Numbers are: " + floridaLottery[0] + " " + floridaLottery[1] + " " + floridaLottery[2] + 
-												" " + floridaLottery[3] + " " + floridaLottery[4] + " " + floridaLottery[5]);
-  // else if they want the PowerBall Lottery		
-} else if (userPrompt == 'pb') {
-	// call the powerBallLottery function passing the powerBallLottery array
-	playPowerBallLottery(powerBallLottery);
-	// Log to the console the results of the array
-	console.log("The PowerBall Numbers are: " + powerBallLottery[0] + " " + powerBallLottery[1] + " " + powerBallLottery[2] + 
-										  " " + powerBallLottery[3] + " " + powerBallLottery[4] + "\n" 
-										      + "And the final PowerBall Number is: " + powerBallLottery[5]);
-}
-
-
 
 
 
